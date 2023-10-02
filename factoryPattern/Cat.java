72503c1344d9293e6d8eb74c6944d50d19bcc52a
Aidan Liam N. Battad <142377920+AidanBattad@users.noreply.github.com>
2023-10-02 11:53:16 +0800

    Update Cat.java

--- factoryPattern/Cat.java
+++ factoryPattern/Cat.java
@@ -1,23 +1,7 @@
-package factoryPattern;
+package factoryPattern2;
 
-public class Cat implements Pet {
-    private Integer noOfLives;
-
-    public Integer getNoOfLives() {
-        return noOfLives;
-    }
-
-    public void setNoOfLives(Integer noOfLives) {
-        this.noOfLives = noOfLives;
-    }
-
-    @Override
-    public String makeSound() {
-        return "Meow, meow!";
-    }
-
-    @Override
-    public String play() {
-        return "Catching/pursuing moving objects!";
-    }
+public interface Pet {
+    String makeSound();
+    String play();
+    void displayFactoryChange();
 }
